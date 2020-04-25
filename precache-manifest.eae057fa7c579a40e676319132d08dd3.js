a7e1e335c6f109cf1d70a1d983ff690ecc565ebc
shivam <developer.sp4799@gmail.com>
2020-04-25 13:28:20 +0530

    Updates

---
+++ precache-manifest.eae057fa7c579a40e676319132d08dd3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045f316cd868401fdb9950fa5e3f50e8",
+    "url": "/react-resto/index.html"
+  },
+  {
+    "revision": "8e2093886f858c173a16",
+    "url": "/react-resto/static/css/main.805a4d7f.chunk.css"
+  },
+  {
+    "revision": "7cfb175ffe87bd173720",
+    "url": "/react-resto/static/js/2.a9ea4cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-resto/static/js/2.a9ea4cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2093886f858c173a16",
+    "url": "/react-resto/static/js/main.3fc69658.chunk.js"
+  },
+  {
+    "revision": "f6f2bb4cb353c68966e7",
+    "url": "/react-resto/static/js/runtime-main.1fe791b5.js"
+  },
+  {
+    "revision": "fcbaeba41efa77a0128ade0af7785569",
+    "url": "/react-resto/static/media/foof.fcbaeba4.jpg"
+  },
+  {
+    "revision": "278cde59861a12c2fba1b6559788a029",
+    "url": "/react-resto/static/media/img1.278cde59.jpg"
+  },
+  {
+    "revision": "dfad2e0026b143267ceef437a2e30c15",
+    "url": "/react-resto/static/media/img_square_1.dfad2e00.jpg"
+  },
+  {
+    "revision": "45ed6f03eb5e89dc705692215077364f",
+    "url": "/react-resto/static/media/img_square_2.45ed6f03.jpg"
+  },
+  {
+    "revision": "278cde59861a12c2fba1b6559788a029",
+    "url": "/react-resto/static/media/img_square_3.278cde59.jpg"
+  },
+  {
+    "revision": "4b84131d1b1ebe268760e56a84a8182a",
+    "url": "/react-resto/static/media/img_square_4.4b84131d.jpg"
+  },
+  {
+    "revision": "018cd4dd32ede2cc3c43572ee64eba44",
+    "url": "/react-resto/static/media/img_square_5.018cd4dd.jpg"
+  },
+  {
+    "revision": "e0b7cf383018fe41d4151ec249bbcce5",
+    "url": "/react-resto/static/media/landing1.e0b7cf38.png"
+  },
+  {
+    "revision": "0721c80445de3c54df59fdd79303b0aa",
+    "url": "/react-resto/static/media/landing2.0721c804.png"
+  },
+  {
+    "revision": "ac51732a2f7878b584845a81d5907c80",
+    "url": "/react-resto/static/media/landing3.ac51732a.png"
+  },
+  {
+    "revision": "1d7b217069abe4b68609f7d4b3594a44",
+    "url": "/react-resto/static/media/landing4.1d7b2170.png"
+  },
+  {
+    "revision": "a9a75cddb6b73882512f3fa7bae5b91d",
+    "url": "/react-resto/static/media/market.a9a75cdd.jpg"
+  },
+  {
+    "revision": "b8c25d77f400334f939b63f4a267c14a",
+    "url": "/react-resto/static/media/seats.b8c25d77.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-react-app/precache-manifest.31aff8ecf079ef08d22df6fbef6139b5.js"
+  "/react-resto/precache-manifest.eae057fa7c579a40e676319132d08dd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-resto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
